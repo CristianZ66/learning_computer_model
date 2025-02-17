145b483c4df95ce1ebf06b38a4a9c7fdcda23453
CristianZ66 <kristianzaa2@gmail.com>
2025-02-17 11:29:54 -0500

    se agrego una funcion de modulo

--- calculadora.py
+++ calculadora.py
@@ -3,12 +3,17 @@ print("::Calculadora basica ::")
 num1 = 20
 num2 = 15
 
+
+
 suma = num1+num2
 resta = num1-num2
 divicion = num1/num2
 multiplicacion = num1*num2
+modulo = (num1*num2)/num2
+
 
 print("la suma de los dos numeros es ",suma)
 print("la resta de los dos numeros es ",resta)
 print("la divi de los dos numeros es ",divicion)
 print("la multiplicacion de los dos numeros es ",multiplicacion)
+print("El modulo de los tres numeros es ",modulo)
